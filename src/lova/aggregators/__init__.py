7e4617311d50264dafdc06e4eeb39df18b4b208e
chengyin <ycheng_sjtu@foxmail.com>
2024-01-12 15:02:56 +0800

    Add function to merge interaction data with strength values, computing a new 'strength' column in DataFrame

--- src/lova/aggregators/__init__.py
+++ src/lova/aggregators/__init__.py
@@ -2,10 +2,12 @@ from .generate_sequence_order import (
     sequence_order_from_date,
     sequence_order_from_date_with_time_dacay,
 )
+from .merge_with_strength import merge_interactions_with_strength
 from .reduce_with_binary_or import calculate_binary_or
 
 __all__ = [
     "calculate_binary_or",
+    "merge_interactions_with_strength",
     "sequence_order_from_date_with_time_dacay",
     "sequence_order_from_date",
 ]
---
+++ src/lova/aggregators/merge_with_strength.py
@@ -0,0 +1,42 @@
+from typing import Dict
+
+import numpy as np
+import pandas as pd
+
+
+def merge_interactions_with_strength(
+    interactions: pd.DataFrame,
+    strength_dict: Dict[str, float],
+) -> pd.DataFrame:
+    """
+    Merge interaction data with strength values to compute a combined 'strength' column.
+
+    This function takes a DataFrame of interactions and a dictionary mapping column names to
+    strengths. It computes the sum of each specified column in `interactions` and then creates a
+    new 'strength' column in the DataFrame. This 'strength' column is the dot product of the
+    interaction sums and the strength values.
+
+    Args:
+        interactions (pd.DataFrame): A DataFrame containing interaction data.
+        strength_dict (Dict[str, float]): A dictionary mapping column names in the DataFrame
+                                          to strength values.
+
+    Returns:
+        pd.DataFrame: The modified DataFrame with an additional 'strength' column.
+
+    Examples:
+        >>> interactions = pd.DataFrame({'A': [1, 2], 'B': [3, 4]})
+        >>> strength_dict = {'A': 0.5, 'B': 1.5}
+        >>> merge_interactions_with_strength(interactions, strength_dict)
+    """
+    selected_columns = []
+    strength_list = []
+    for key, value in strength_dict.items():
+        selected_columns.append(key)
+        strength_list.append(value)
+    print(selected_columns)
+    print(strength_list)
+    for col in selected_columns:
+        interactions[col] = interactions[col].apply(sum)
+    interactions["strength"] = np.dot(interactions[selected_columns], strength_list)
+    return interactions
