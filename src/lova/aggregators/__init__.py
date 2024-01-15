3e239f7c5b76f8a3efbf9271f9331249c91651a2
chengyin <ycheng_sjtu@foxmail.com>
2024-01-15 12:19:48 +0800

    Convert list of integers to binary vector using bit-shifting and bitwise AND, based on 'field_num' length

--- src/lova/aggregators/__init__.py
+++ src/lova/aggregators/__init__.py
@@ -1,3 +1,4 @@
+from .convert_label_encoding_to_vector import label_list_to_vector, label_to_vector
 from .generate_sequence_order import (
     sequence_order_from_date,
     sequence_order_from_date_with_time_dacay,
@@ -8,6 +9,8 @@ from .reduce_with_binary_or import calculate_binary_or
 
 __all__ = [
     "calculate_binary_or",
+    "label_list_to_vector",
+    "label_to_vector",
     "merge_numerical_interactions_with_strength",
     "normalize_with_percentile_cap",
     "sequence_order_from_date_with_time_dacay",
---
+++ src/lova/aggregators/convert_label_encoding_to_vector.py
@@ -0,0 +1,44 @@
+from typing import List
+
+import numpy as np
+
+
+def label_to_vector(label: List[int], field_num: int) -> np.ndarray:
+    """
+    Convert single-element integer list to binary vector using bit-shifting, bounded by 'field_num'
+
+    This function takes a label, represented as a list of integers, and converts it
+    into a binary vector. The conversion is done by bit-shifting each integer in the label
+    and applying a bitwise AND operation. The resulting binary vector has a length
+    specified by 'field_num'.
+
+    Args:
+        label (List[int]): A list of integers representing the label.
+        field_num (int): The length of the binary vector representation.
+
+    Returns:
+        np.ndarray: A numpy array representing the label in binary vector form.
+    """
+    return np.array([(label[0] >> i) & 1 for i in range(field_num - 1, -1, -1)])
+
+
+def label_list_to_vector(label_list: List[List[int]], field_num: int) -> np.ndarray:
+    """
+    Converts a list of labels to a vector representation.
+
+    This function takes a list of labels, each represented as a list of integers, and
+    converts them into a vector representation. It does so by calling 'label_to_vector'
+    for each label in the list and summing the resulting vectors. The length of the
+    vector representation is specified by 'field_num'.
+
+    Args:
+        label_list (List[List[int]]): A list of labels, each label is a list of integers.
+        field_num (int): The length of the binary vector representation for each label.
+
+    Returns:
+        np.ndarray: A numpy array representing the summed vector of all labels.
+    """
+    vec = np.zeros(field_num)
+    for label in label_list:
+        vec += label_to_vector(label, field_num)
+    return vec
