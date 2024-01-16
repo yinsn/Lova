d81cb4fe1b87422537debb6b96a95cde79d7234c
chengyin <ycheng_sjtu@foxmail.com>
2024-01-16 17:40:51 +0800

    Created an abstract 'BasePreprocessor' class with essential structure and preprocess method for data preparation

--- src/lova/preprocessors/base.py
+++ src/lova/preprocessors/base.py
@@ -4,6 +4,19 @@ import pandas as pd
 
 
 class BasePreprocessor(metaclass=ABCMeta):
+    """
+    An abstract base class for preprocessing datasets.
+
+    This class is intended to be subclassed with specific implementations of
+    the `preprocess` method for different preprocessing strategies.
+
+    Attributes:
+        user_column (str): The name of the user column in the dataset.
+        item_column (str): The name of the item column in the dataset.
+        label_column (str): The name of the label column in the dataset.
+        dataset (pd.DataFrame): The dataset to be preprocessed.
+    """
+
     def __init__(
         self,
         user_column: str,
@@ -11,6 +24,15 @@ class BasePreprocessor(metaclass=ABCMeta):
         label_column: str,
         dataset: pd.DataFrame,
     ) -> None:
+        """
+        Initializes the BasePreprocessor with the dataset and column names.
+
+        Args:
+            user_column (str): The name of the user column in the dataset.
+            item_column (str): The name of the item column in the dataset.
+            label_column (str): The name of the label column in the dataset.
+            dataset (pd.DataFrame): The dataset to be preprocessed.
+        """
         self.user_column = user_column
         self.item_column = item_column
         self.label_column = label_column
@@ -18,4 +40,13 @@ class BasePreprocessor(metaclass=ABCMeta):
 
     @abstractmethod
     def preprocess(self) -> None:
+        """
+        Abstract method for preprocessing the dataset.
+
+        This method should be implemented by subclasses to apply specific preprocessing steps.
+        The implementation should modify the `self.dataset` attribute in-place.
+
+        Raises:
+            NotImplementedError: If the method is not overridden in a subclass.
+        """
         raise NotImplementedError
