086208fc2cbcd443ea230b12625e14871c11537a
chengyin <ycheng_sjtu@foxmail.com>
2024-01-17 17:29:02 +0800

    Add 'load_config' function to load YAML configuration, defaults to 'config.yml' if no path specified

--- src/lova/dataloaders/__init__.py
+++ src/lova/dataloaders/__init__.py
@@ -1,4 +1,5 @@
 from .base import BaseDataLoader
+from .load_config import load_config
 from .load_sequence import SequenceLoader
 
-__all__ = ["BaseDataLoader", "SequenceLoader"]
+__all__ = ["BaseDataLoader", "load_config", "SequenceLoader"]
