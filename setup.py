d37586f65b45613b9d541ebb94da4de5e8b59f31
chengyin <ycheng_sjtu@foxmail.com>
2024-01-18 17:00:29 +0800

    Add dependencies for 'mpi4py' and 'tqdm'

--- setup.py
+++ setup.py
@@ -11,9 +11,11 @@ install_requires = (
     [
         "implicit>=0.7.2",
         "joblib>=1.3.2",
+        "mpi4py>=3.1.5",
         "optuna>=3.0.6",
         "pyarrow>=14.0.1",
         "pyyaml>=6.0",
+        "tqdm>=4.66.1",
     ],
 )
 
