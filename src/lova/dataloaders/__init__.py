b62b629da68ff11394e77004300c67da71c455b2
chengyin <ycheng_sjtu@foxmail.com>
2024-01-22 15:18:35 +0800

    Add DataFrameLoader class constructor with initialization parameters for file loading and configuration

--- src/lova/dataloaders/__init__.py
+++ src/lova/dataloaders/__init__.py
@@ -1,6 +1,7 @@
 from .base import BaseDataLoader
 from .download_hdfs import HDFSDownloader
 from .load_config import load_config
+from .load_dataframe import DataFrameLoader
 from .load_hdfs import HDFSDataloader
 from .load_sequence import SequenceLoader
 from .logical_processors import get_logical_processors_count
@@ -8,6 +9,7 @@ from .set_path import ensure_study_directory
 
 __all__ = [
     "BaseDataLoader",
+    "DataFrameLoader",
     "ensure_study_directory",
     "get_logical_processors_count",
     "HDFSDataloader",
---
+++ src/lova/dataloaders/load_dataframe.py
@@ -0,0 +1,98 @@
+import logging
+import os
+from typing import Dict, Optional, Union
+
+import pandas as pd
+
+from .base import BaseDataLoader
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
+
+
+class DataFrameLoader(BaseDataLoader):
+    def __init__(
+        self,
+        column_names: pd.Index,
+        file_path: Optional[str] = None,
+        file_name: Optional[str] = None,
+        file_type: str = "pkl",
+        max_rows: Optional[int] = None,
+        config: Optional[Dict] = None,
+    ) -> None:
+        """
+        Initializes the DataFrameLoader with specified parameters.
+
+        Args:
+            column_names (pd.Index): Index object representing the column names of the DataFrame.
+            file_path (Optional[str]): Path of the file to be loaded. Defaults to None.
+            file_name (Optional[str]): Name of the file to be loaded. Defaults to None.
+            file_type (str): Type of the file to be loaded (e.g., 'csv', 'pkl'). Defaults to 'pkl'.
+            max_rows (Optional[int]): Maximum number of rows to load from the file. Defaults to None.
+            config (Optional[Dict]): Configuration parameters for data loading. Defaults to None.
+        """
+        self.column_names = column_names
+        super().__init__(file_path, file_name, file_type, max_rows, config)
+        self.file_url = (
+            os.path.join(self.file_path, self.file_name) + "." + self.file_type
+        )
+
+    @staticmethod
+    def convert_string_to_tuple_of_num(value: Union[int, str]) -> tuple:
+        """
+        Convert a string or an integer into a tuple. If the input is a string, it
+        is assumed to contain elements separated by '\x02'. Each element is either
+        converted to an integer (if possible) or remains a string.
+
+        Args:
+            value (Union[int, str]): An integer or a string containing elements
+                                    separated by '\x02'.
+
+        Returns:
+            Tuple[Union[int, str], ...]: A tuple where each element is either an
+                                        integer or a string.
+        """
+        if isinstance(value, int):
+            return (value,)
+        else:
+            elements = value.split("\x02")
+            return tuple(
+                int(element) if element.isdigit() else element for element in elements
+            )
+
+    def _get_dataframe(self) -> None:
+        """
+        Loads data from a file URL into a pandas DataFrame.
+        """
+        logger.info("Loading data from %s", self.file_url)
+        data = pd.read_pickle(self.file_url)
+        if isinstance(data, pd.DataFrame):
+            self.dataframe = data
+            self.dataframe.columns = self.column_names
+        else:
+            logger.error("Failed to load data from %s", self.file_url)
+            self.dataframe = pd.DataFrame()
+
+    def _literal_dataframe(self) -> None:
+        """
+        Convert string representations in each column of the dataframe to their literal structures.
+        """
+        logger.info("Converting string representations to literal structures")
+        for col in self.column_names:
+            self.dataframe[col] = self.dataframe[col].apply(
+                self.convert_string_to_tuple_of_num
+            )
+
+    def load_data(self) -> pd.DataFrame:
+        """
+        Loads data into a DataFrame, processes it, and returns the resulting DataFrame.
+
+        Returns:
+            pd.DataFrame: The loaded and processed DataFrame.
+        """
+        self._get_dataframe()
+        self._literal_dataframe()
+        logger.info("Data loading finished")
+        return self.dataframe
